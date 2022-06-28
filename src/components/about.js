1645283c88098a86fc004f316f4d054dc2c7b9d5
mattfreshwaters <mattfreshy@gmail.com>
2022-06-28 11:17:26 -0400

    css

--- src/components/about.js
+++ src/components/about.js
@@ -1,18 +1,13 @@
 import React from 'react';
 
-const divStyle = {
-    color:'white'
-};
-
 class About extends React.Component {
     
     render() {
         return (
-            <div style={divStyle}>
-                <h2>About Page</h2>
-                <main>
-                    <p>This section contains information about...</p>
-                </main>
+            <div className="about">
+                <header className="App-header">
+                    header
+                </header>
             </div>
         )
     }
